cdbd1f8ab0b60b343e3e698c7670699e1f0efaf3
grigorov8 <126501345+grigorov8@users.noreply.github.com>
2023-05-23 10:17:28 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace GuessANumber
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            Random randomNumber = new Random();
+
+            int computerNumber = randomNumber.Next(1, 100);
+
+            while(true)
+            {
+                Console.Write("Guess a number (1-100): ");
+
+
+                string playerInput = Console.ReadLine();
+
+                bool IsValyd = int.TryParse(playerInput, out int playerNumber);
+
+                if (IsValyd)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    
+
+                    else if (playerNumber > computerNumber)
+                    {
+                        Console.WriteLine("Its too High");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Its too Low");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
